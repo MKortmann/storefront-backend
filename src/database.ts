6911d4f90e160a1da93630ecc4fab3a55e37432f
MKortmann <companymvms@gmail.com>
2023-07-27 22:17:14 +0200

    fix: connection between pg from localhost to the container at localhost exposed port 5433

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-/* pg module is a popular Node.js library used to interact with PostgresSQL databases.
+/* pg module is a popular Node.js library used to interact with PostgreSQL databases.
 The pg module provides an interface for connecting to, querying, and managing PostgreSQL databases from Node.js applications.
 
 The 'Pool' class is an essential part of the 'pg' module. It represents a pool of client connections to the PostgreSQL database. When you want to execute queries, you typically need to establish a connection to the database server. However, establishing a new connection for every query can be resource-intensive and slow. To address this, connection pooling is used.
@@ -14,6 +14,7 @@ const {
   POSTGRES_TEST_DB,
   POSTGRES_USER,
   POSTGRES_PASSWORD,
+  POSTGRES_PORT,
   ENV,
 } = process.env;
 
@@ -25,6 +26,7 @@ if (ENV === 'test') {
     database: POSTGRES_TEST_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
+    port: 5433,
   });
 }
 
@@ -34,6 +36,7 @@ if (ENV === 'dev') {
     database: POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
+    port: parseInt(POSTGRES_PORT || '5432'),
   });
 }
 

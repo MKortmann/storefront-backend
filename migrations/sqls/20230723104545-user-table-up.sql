ff33cc95ce47b055171054d760c27cd50dc92e61
MKortmann <companymvms@gmail.com>
2023-12-05 12:41:17 +0100

    add: test for authenticate and remove error.log and add it in gitignore

--- migrations/sqls/20230723104545-user-table-up.sql
+++ migrations/sqls/20230723104545-user-table-up.sql
@@ -9,6 +9,7 @@ CREATE TABLE if not EXISTS users (
 -- Just Sample Data to be Able to Test Fully
 INSERT INTO users (firstname, lastname, email, password_digest)
 VALUES
-  ('John', 'Doe', 'john.doe@example.com', '$2y$10$olR7IzAHyjBKsWnyneAXbO.SZaj4t4ueUksA2i0OPQeAzMlUM2qB.'),
-  ('Jane', 'Smith', 'jane.smith@example.com', '$2y$10$olR7IzAHyjBKsWnyneAXbO.SZaj4t4ueUksA2i0OPQeAzMlUM2qB.'),
-  ('Alice', 'Johnson', 'alice.johnson@example.com', '$2y$10$olR7IzAHyjBKsWnyneAXbO.SZaj4t4ueUksA2i0OPQeAzMlUM2qB.');
+  ('John', 'Doe', 'john.doe@example.com', '$2b$10$34vVRl6sVjGtf1YTXd8q7ea7LvcWMMyum6s98GaDB5Eb51AQ6ns5m'),
+  ('Jane', 'Smith', 'jane.smith@example.com', '$2b$10$34vVRl6sVjGtf1YTXd8q7ea7LvcWMMyum6s98GaDB5Eb51AQ6ns5m'),
+  ('Alice', 'Johnson', 'alice.johnson@example.com', '$2b$10$34vVRl6sVjGtf1YTXd8q7ea7LvcWMMyum6s98GaDB5Eb51AQ6ns5m'),
+  ('Alice2', 'Johnson', 'alice.johnson@example.com', '$2b$10$34vVRl6sVjGtf1YTXd8q7ea7LvcWMMyum6s98GaDB5Eb51AQ6ns5m');
--- src/tests/models/userStoreSpec.ts
+++ src/tests/models/userStoreSpec.ts
@@ -1,6 +1,7 @@
 import { User, UserStore } from '../../models/user';
 import { logger, logTestStart, logTestFinish, logTestError } from '../../logger';
 import 'jasmine-expect';
+import { QueryResult } from 'pg';
 
 const specUserStore = 'userStoreSpec.ts';
 
@@ -14,31 +15,31 @@ describe(specUserStore, () => {
 
   afterAll(() => {});
 
-  const describeIndexMethod = 'index method';
-  describe(describeIndexMethod, () => {
-    const itDescription = 'should fetch users and have the expected properties';
-    it(itDescription, async () => {
-      try {
-        logTestStart(specUserStore, describeIndexMethod, itDescription);
-        const users = await userStore.index();
-        logger.warn(`users at index method: ${JSON.stringify(users)}`);
-        expect(Array.isArray(users)).toBe(true);
-        expect(users.length).toBeGreaterThan(0);
-        users.forEach((user, index) => {
-          expect(user['id']).toBeDefined();
-          expect(user['firstname']).toBeDefined();
-          expect(user['lastname']).toBeDefined();
-          expect(user['email']).toBeDefined();
-          expect(user['password_digest']).toBeDefined();
-        });
+  // const describeIndexMethod = 'index method';
+  // describe(describeIndexMethod, () => {
+  //   const itDescription = 'should fetch users and have the expected properties';
+  //   it(itDescription, async () => {
+  //     try {
+  //       logTestStart(specUserStore, describeIndexMethod, itDescription);
+  //       const users = await userStore.index();
+  //       logger.warn(`users at index method: ${JSON.stringify(users)}`);
+  //       expect(Array.isArray(users)).toBe(true);
+  //       expect(users.length).toBeGreaterThan(0);
+  //       users.forEach((user, index) => {
+  //         expect(user['id']).toBeDefined();
+  //         expect(user['firstname']).toBeDefined();
+  //         expect(user['lastname']).toBeDefined();
+  //         expect(user['email']).toBeDefined();
+  //         expect(user['password_digest']).toBeDefined();
+  //       });
 
-        logTestFinish(specUserStore, describeIndexMethod, itDescription);
-      } catch (err: unknown) {
-        logTestError(specUserStore, describeIndexMethod, itDescription, err);
-        throw err;
-      }
-    });
-  });
+  //       logTestFinish(specUserStore, describeIndexMethod, itDescription);
+  //     } catch (err: unknown) {
+  //       logTestError(specUserStore, describeIndexMethod, itDescription, err);
+  //       throw err;
+  //     }
+  //   });
+  // });
 
   // const describeShowMethod = 'show method';
   // describe(describeShowMethod, () => {
@@ -60,10 +61,8 @@ describe(specUserStore, () => {
   //         'firstname',
   //         'lastname',
   //         'email',
-  //         'password',
+  //         'password_digest',
   //       ];
-  //       console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-  //       console.log(`${JSON.stringify(user)}`);
   //       expectedProperties.forEach((property: keyof User) => {
   //         expect(user[property]).toBeDefined();
   //       });
@@ -91,7 +90,7 @@ describe(specUserStore, () => {
   //         firstname: 'Carlos',
   //         lastname: 'Judas',
   //         email: 'carlos@gmail.com',
-  //         password: '1234',
+  //         password_digest: '1234',
   //       };
   //       const createdUser: User = await userStore.create(newUser);
 
@@ -99,6 +98,7 @@ describe(specUserStore, () => {
   //       expect(createdUser.firstname).toBe('Carlos');
   //       expect(createdUser.lastname).toBe('Judas');
   //       expect(createdUser.email).toEqual('carlos@gmail.com');
+  //       expect(createdUser.password_digest).toBeDefined();
 
   //       logger.warn(
   //         `${specUserStore} - ${describeCreateMethod}: ${itDescription} - successfully finished`
@@ -132,6 +132,70 @@ describe(specUserStore, () => {
   //     }
   //   });
   // });
+
+  // const describeUpdateMethod = 'update method';
+  // describe(describeUpdateMethod, () => {
+  //   const itDescription = 'should update a specific user by an ID';
+  //   it(itDescription, async () => {
+  //     try {
+  //       logger.warn(
+  //         `${specUserStore} - ${describeUpdateMethod}: ${itDescription} - started`
+  //       );
+  //       const userId = '1';
+
+  //       const updateUser = {
+  //         firstname: 'John',
+  //         lastname: 'Doe',
+  //         email: 'john.doe@yahoo.com',
+  //         password_digest: '123456789',
+  //       };
+
+  //       const updatedUser: QueryResult = await userStore.update(userId, updateUser);
+
+  //       expect(updatedUser.rowCount).toBe(1);
+  //     } catch (err) {
+  //       logger.error(
+  //         `${specUserStore} - ${describeUpdateMethod}: ${itDescription} - failed: ${err}`
+  //       );
+  //     }
+  //   });
+  // });
+
+  /**
+   * STOPPED HERE
+   */
+  const describeAuthenticateMethod = 'authenticate method';
+  describe(describeAuthenticateMethod, () => {
+    const itDescription = 'should authenticate an user with email and password';
+    it(itDescription, async () => {
+      try {
+        logger.warn(
+          `${specUserStore} - ${describeAuthenticateMethod}: ${itDescription} - started`
+        );
+
+        //123456 is equal or should be equal the password saved in the create-user-table
+        const login = {
+          email: 'john.doe@example.com',
+          password: '123456',
+        };
+
+        const authenticate: User | null = await userStore.authenticate(login);
+
+        expect(authenticate?.email).toEqual('john.doe@example.com');
+        expect(authenticate?.firstname).toEqual('John');
+        expect(authenticate?.lastname).toEqual('Doe');
+        expect(authenticate?.password_digest).toEqual(
+          '$2b$10$34vVRl6sVjGtf1YTXd8q7ea7LvcWMMyum6s98GaDB5Eb51AQ6ns5m'
+        );
+
+        logger.warn(JSON.stringify(authenticate));
+      } catch (err) {
+        logger.error(
+          `${specUserStore} - ${describeAuthenticateMethod}: ${itDescription} - failed: ${err}`
+        );
+      }
+    });
+  });
 
   // const describeOrderByUserMethod = 'currentOrderByUser method';
   // describe(describeOrderByUserMethod, () => {
